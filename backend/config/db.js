aa8da5c925a84ca2f226e61b5b403de38faf42c2
Ammar8207 <ammar.hashmi110@gmail.com>
2025-10-13 21:22:15 +0500

    update backend code and configs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg"); // Change to pg for PostgreSQL
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const db = mysql.createPool({
+const db = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 5432, // PostgreSQL default
 });
 
 module.exports = db;
